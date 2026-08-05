05fded58e2e36ec91aa67b6d26de61cc8e3107ff
HippoMaiDinh <lym861@hsd.k12.or.us>
2026-08-05 00:13:19 -0700

    adding first file project

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Dự án này được tạo ra để chúc mừng sinh nhật một cô gái hết sức tuyệt vời
+Đó là Chị Huong Le
+Được tạo bởi Man Ly
+"""
+
+message = "Happy Birthday, Chị Hương"
+
+if __name__ == "__main__":
+    print(message)
